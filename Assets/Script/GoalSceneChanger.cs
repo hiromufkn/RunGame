b95b2494d90f0058e0c766990d783205823a130f
吹野拓夢 <kof2564720@stu.o-hara.ac.jp>
2026-05-27 10:46:34 +0900

    シーンチェンジのスクリプト作成開始

--- Assets/Script/GoalSceneChanger.cs
+++ Assets/Script/GoalSceneChanger.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GoalSceneChanger : MonoBehaviour
 {
@@ -14,5 +15,26 @@ public class GoalSceneChanger : MonoBehaviour
     void Update()
     {
         
+    }
+
+    [SerializeField] private string sceneA;
+    [SerializeField] private string sceneB;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // 0‚©1‚ðƒ‰ƒ“ƒ_ƒ€Žæ“¾
+            int randomValue = Random.Range(0, 2);
+
+            if (randomValue == 0)
+            {
+                SceneManager.LoadScene(sceneA);
+            }
+            else
+            {
+                SceneManager.LoadScene(sceneB);
+            }
+        }
     }
 }
